93a324a8a43f2c7639754cb7ab116419669ae5c6
Jvictor940 <98998587+Jvictor940@users.noreply.github.com>
2023-11-28 02:21:22 -0500

    added css to our congrats page, and contactinformation page, also changed the image to the lineman on our contactInfo page

--- src/components/registration/congrats.jsx
+++ src/components/registration/congrats.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import './Congrats.css'
 
 const Congrats = () => {
     return(
-        <div>
-            <h1>Conratulations</h1>
-            <h3>We'll see you on xxxx</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate saepe veniam distinctio accusantium ea quisquam asperiores aspernatur? Assumenda repellendus ipsum, aliquam pariatur magni laborum possimus cupiditate. Veniam quos accusantium quae?</p>
+        <div className="img">
+            <div className="text">
+                <h1>Conratulations</h1>
+                <h3>We'll see you on xxxx</h3>
+                <p className="congrats-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate saepe veniam distinctio accusantium ea quisquam asperiores aspernatur? Assumenda repellendus ipsum, aliquam pariatur magni laborum possimus cupiditate. Veniam quos accusantium quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta blanditiis commodi aut eos fuga totam! Voluptates suscipit natus maiores a numquam fuga cupiditate, eius illo labore facere amet, blanditiis vero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id itaque quae accusantium ipsam consequatur numquam ipsa repudiandae at soluta illum. At totam nobis cum quidem, officia iste dolore. Expedita, excepturi.</p>
+            </div>
         </div>
     )
 }
